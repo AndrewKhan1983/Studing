5f8ecdc4e7b1d5a6c2edbbaada8d4300d149faf4
Andrew Khan <andre-khan@yandex.ru>
2021-06-14 21:38:37 +0300

    домашка 14.06.21

--- src/lesson13_1/CollectionUtilsImpl.java
+++ src/lesson13_1/CollectionUtilsImpl.java
@@ -1,42 +1,88 @@
 package lesson13_1;
 
-import java.util.Collection;
-import java.util.LinkedList;
-import java.util.Set;
+import java.util.*;
 
 public class CollectionUtilsImpl implements CollectionUtils {
     @Override
     public Collection<Integer> union(Collection<Integer> a, Collection<Integer> b) throws NullPointerException {
-        return null;
+
+
+        LinkedList<Integer> union = new LinkedList<>();
+        union.addAll(a);
+
+        for (Integer e : b) {
+            if (!union.contains(e)) ;
+            union.add(e);
+
+
+        }
+        return union;
+
     }
 
     @Override
     public Collection<Integer> intersection(Collection<Integer> a, Collection<Integer> b) throws NullPointerException {
-        return null;
+        LinkedList<Integer> intersection = new LinkedList<>();
+        for (Integer num : a) {
+            if (b.contains(num)) {
+                intersection.add(num);
+
+            }
+
+        }
+        for (Integer num : b) {
+            if (a.contains(num)) {
+                intersection.add(num);
+
+            }
+
+        }
+
+        return intersection;
     }
 
     @Override
     public Set<Integer> unionWithoutDuplicate(Collection<Integer> a, Collection<Integer> b) throws NullPointerException {
-        return null;
+        Set<Integer> num = new LinkedHashSet<>(a);
+        num.addAll(b);
+        List<Integer> finnum = new LinkedList<>(num);
+
+
+        return num;
     }
 
     @Override
     public Set<Integer> intersectionWithoutDuplicate(Collection<Integer> a, Collection<Integer> b) throws NullPointerException {
-        return null;
+
+        Set<Integer> intersectionWithoutDuplicate = new LinkedHashSet<>();
+        for (Integer num : a) {
+            if (b.contains(num)) ;
+            intersectionWithoutDuplicate.add(num);
+
+        }
+
+        for (Integer num : b) {
+            if (a.contains(num)) {
+                intersectionWithoutDuplicate.add(num);
+
+            }
+
+        }
+        return intersectionWithoutDuplicate;
     }
 
     @Override
     public Collection<Integer> difference(Collection<Integer> a, Collection<Integer> b) throws NullPointerException {
         LinkedList<Integer> difference = new LinkedList<>();
-        for (Integer num: a) {
-            if (!b.contains(num)){
+        for (Integer num : a) {
+            if (!b.contains(num)) {
                 difference.add(num);
 
             }
 
         }
-        for (Integer num:b) {
-            if (!a.contains(num)){
+        for (Integer num : b) {
+            if (!a.contains(num)) {
                 difference.add(num);
 
             }
