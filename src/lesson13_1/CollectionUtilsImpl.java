ff36dc0df7a1b3a1e4e47429f02143cfbe91502f
Андрей Хан <81438426+AndrewKhan1983@users.noreply.github.com>
2021-06-17 11:40:28 +0300

    Update CollectionUtilsImpl.java

--- src/lesson13_1/CollectionUtilsImpl.java
+++ src/lesson13_1/CollectionUtilsImpl.java
@@ -17,76 +17,56 @@ public class CollectionUtilsImpl implements CollectionUtils {
 
         }
         return union;
-
     }
-
     @Override
     public Collection<Integer> intersection(Collection<Integer> a, Collection<Integer> b) throws NullPointerException {
         LinkedList<Integer> intersection = new LinkedList<>();
         for (Integer num : a) {
             if (b.contains(num)) {
                 intersection.add(num);
-
             }
-
         }
         for (Integer num : b) {
             if (a.contains(num)) {
                 intersection.add(num);
-
             }
-
         }
-
         return intersection;
     }
-
     @Override
     public Set<Integer> unionWithoutDuplicate(Collection<Integer> a, Collection<Integer> b) throws NullPointerException {
         Set<Integer> nu = new LinkedHashSet<>(a);
         nu.addAll(b);
         List<Integer> finnum = new LinkedList<>(nu);
 
-
         return nu;
     }
-
     @Override
     public Set<Integer> intersectionWithoutDuplicate(Collection<Integer> a, Collection<Integer> b) throws NullPointerException {
-
         Set<Integer> intersectionWithoutDuplicate = new LinkedHashSet<>();
         for (Integer num : a) {
             if (b.contains(num)) ;
             intersectionWithoutDuplicate.add(num);
-
         }
-
         for (Integer num : b) {
             if (a.contains(num)) {
                 intersectionWithoutDuplicate.add(num);
-
             }
-
         }
         return intersectionWithoutDuplicate;
     }
-
     @Override
     public Collection<Integer> difference(Collection<Integer> a, Collection<Integer> b) throws NullPointerException {
         LinkedList<Integer> difference = new LinkedList<>();
         for (Integer num : a) {
             if (!b.contains(num)) {
                 difference.add(num);
-
             }
-
         }
         for (Integer num : b) {
             if (!a.contains(num)) {
                 difference.add(num);
-
             }
-
         }
         return difference;
     }
