cd7a85779694db8d15d1352e58f8270850d5f798
Андрей Хан <81438426+AndrewKhan1983@users.noreply.github.com>
2021-06-17 11:35:46 +0300

    Update Main.java

--- src/Home10/Main.java
+++ src/Home10/Main.java
@@ -7,19 +7,13 @@ public class Main {
         Tovar tovar1 = new Tovar(120, "Мышка",3.7);
         Tovar tovar2 = new Tovar(500, "Клава",4.7);
         Tovar tovar3 = new Tovar(30000, "Моно",5);
-
         TreeSet<Tovar> tovars = new TreeSet<>();
         tovars.add(tovar1);
         tovars.add(tovar2);
         tovars.add(tovar3);
-
         TreeSet<Tovar> sortedByName = TovarUtils.sortByName(tovars);
-
         for (Tovar t:sortedByName) {
             System.out.println(t);
-
         }
-
-
     }
 }
